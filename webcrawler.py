be55a9b67f38ca3f219a2339a0170ef57bccf0ef
yfliu87 <yfliu87@gmail.com>
2015-07-02 11:21:45 +0800

    update comment

---
+++ webcrawler.py
@@ -0,0 +1,10 @@
+'''
+initial version of webcrawler
+The first version plans to do below things:
+1. extract urls from seed page within specific depth
+2. index urls to its content
+3. simple query
+
+Future work:
+performance improvement
+'''
