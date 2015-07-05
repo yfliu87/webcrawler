0302330827460bba67fa408d9e4897df80b9404c
Yifei Liu <yfliu87@gmail.com>
2015-07-05 16:09:26 +0800

    refactor find url method

--- webcrawler.py
+++ webcrawler.py
@@ -9,9 +9,9 @@ Future work:
 performance improvement
 '''
 
-def find_url(page):
+def get_next_target(page):
 	start_link = page.find('<a href=')
-	start_quote = page.find('"', start_link + 1)
+	start_quote = page.find('"', start_link)
 	end_quote = page.find('"', start_quote + 1)
-	url = page[start_quote : end_quote]
-	print url
+	url = page[start_quote+1 : end_quote]
+	print url, end_quote
