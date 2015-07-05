5fdc3c61e9dd998166e8e2c9c16b06c5529db80b
Yifei Liu <yfliu87@gmail.com>
2015-07-05 17:13:42 +0800

    implement hash function based on bucket size

--- hash_index.py
+++ hash_index.py
@@ -1,3 +1,11 @@
 '''
 helper file for replacing list with hashtabel for webpage indexing
 '''
+
+def hash_string(keyword, buckets):
+	ret = 0
+
+	for char in keyword:
+		ret += ord(char)
+
+	return ret%buckets
